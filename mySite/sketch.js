bbca2ef14f38171ed71ad1c27799708eae651740
Abe Avnisan <abraham.avnisan@gmail.com>
2020-11-12 17:37:32 -0500

    first commit

---
+++ mySite/sketch.js
@@ -0,0 +1,11 @@
+/// <reference path="../TSDef/p5.global-mode.d.ts" />
+
+"use strict";
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+}
